21abc086de8237cfe38e2bb4866af722eacbfbf1
SSto <95592688+sstover25@users.noreply.github.com>
2022-01-15 17:34:06 -0600

    button now functions as the call to create the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,10 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-  // MY CODE
-  //I need a code that triggers window.prompts when the Generate Password button is clicked
-  //For each prompt, the user must answer "OK" or "Cancel"
-  //the computer must take the answers for the prompts into account when pulling characters for the password
-  //there must be a library of characters for the computer to pull in order to make a password
-  //For the length prompt, the user must enter a number between 8 and 128 - this is mandatory
-
-
-// Arrays of potential characters
+// Strings of potential characters for the password
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var special = " !#$%&()*+-,./:;<>=?@^`~_";
 
-// The below variables set up the functions to check which character types a user wants to include in the password.
+// Variables and functions that check which character types a user wants to include in the password.
 var lowercaseLetters = true;
 var uppercaseLetters = true;
 var numericCharacters = true;
@@ -66,16 +46,14 @@ var specialCharactersChoice = function() {
   }
 }
 
-// This is the prompt for the password length. Users must input this before receiving their password.
-
 var passLength = "";
 
+// This is the prompt for the password length. Users must input this before receiving their password.
 var getPasswordLength = function() {
   passLength = Number(window.prompt("Please select the desired length of your password. The length must be between 8 to 128 characters."));
   
   // This statement ensures that the password fits within the number range given.
   if (passLength >= 8 && passLength <= 128) {
-    console.log("Your password length will be " + passLength + ".");
     JSON.stringify(passLength);
     return passLength;
   } else {
@@ -85,11 +63,9 @@ var getPasswordLength = function() {
 }
 
 var characterChoices = 0;
+
+// This section adds up to determine how many character types the user selected.
 var characterChoicesNumber = function() {
-
-  // This section adds up to determine how many character types the user selected.
-  // var characterChoices = 0;
-
   if(lowercaseLetters === true) {
     characterChoices = characterChoices + 1;
   }
@@ -104,8 +80,10 @@ var characterChoicesNumber = function() {
   }
 }
 
+let initialPassword = [];
+
 //this is the function that pull together everything above to create the password.
-function generatePassword() {
+var generatePassword = function() {
 
   lowercaseLettersChoice();
   uppercaseLettersChoice();
@@ -118,48 +96,49 @@ function generatePassword() {
     generatePassword();
   } else {
     getPasswordLength();
-    console.log(lowercaseLetters, uppercaseLetters, numericCharacters, specialCharacters, passLength);
   }
 
   characterChoicesNumber();
 
-  console.log(characterChoices);
-
-  var generatedPassword = [];
-
-  // This section instructs the console to pull random items from the strings of the character types that the user has selected. 
+  // This section instructs the console to pull random characters from the strings of the character types that the user has selected. 
   for (var i = 0; i < passLength; ++i) {
     if(lowercaseLetters === true) {
       var num = lowercase.charAt(Math.ceil(Math.random() * (lowercase.length - characterChoices)));
-      generatedPassword.push(num);
+      initialPassword.push(num);
     }
     if(uppercaseLetters === true) {
       var num = uppercase.charAt(Math.ceil(Math.random() * (uppercase.length - characterChoices)));
-      generatedPassword.push(num);
+      initialPassword.push(num);
     }
     if(numericCharacters === true) {
       var num = numbers.charAt(Math.ceil(Math.random() * (numbers.length - characterChoices)));
-      generatedPassword.push(num);
+      initialPassword.push(num);
     }
     if(specialCharacters === true) {
       var num = special.charAt(Math.ceil(Math.random() * (special.length - characterChoices)));
-      generatedPassword.push(num);
+      initialPassword.push(num);
     }
   }
 
-  // This check is added incase the number of characters added above exceeds the inputted password length requirement. If it is, then this action removes the last item from the array.
-  while (generatedPassword.length > passLength) {
-    generatedPassword.pop();
+  // This check is added in case the number of characters added above exceeds the inputted password length requirement. If it is, then this action removes the last item from the array.
+  while (passLength < initialPassword.length) {
+    initialPassword.pop();
   }
 
-  console.log(generatedPassword);
+  // This line changes initialPassword from an Array to a String without commas inbetween all characters. It then returns the value to the generatePassword function.
+  return initialPassword.join('');
 
-  // generatedPassword.toString();
-
-  // console.log(generatedPassword);
 }
 
-generatePassword();
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
